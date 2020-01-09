0a6a7d9498877976f9976f0291c7a1870aecc710
Jarret Logan <jarret.logan@publicissapient.com>
2020-01-09 10:27:41 -0600

    Updates

---
+++ precache-manifest.1f04d95818b773796321e75033133c0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a1b5e9f3643f5a214be8be53b870d5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2401376f7dab9349997a",
+    "url": "/monsters-rolodex/static/css/main.c4b7115b.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2401376f7dab9349997a",
+    "url": "/monsters-rolodex/static/js/main.7a167298.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
